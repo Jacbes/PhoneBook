78c36cb5fb9e6fcb6fa8a3ac17b0357b2fc6530a
Jacbes <skishon100000@gmail.com>
2022-11-03 00:13:48 +0700

    Add import from file

--- src/dev/jacbes/Main.java
+++ src/dev/jacbes/Main.java
@@ -1,5 +1,11 @@
 package dev.jacbes;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Person van = new Person("Van", "Broskov", "88008004455", "Novosibirsk");
@@ -8,5 +14,28 @@ public class Main {
         eltex.call();
         System.out.println(van);
         System.out.println(eltex);
+
+        try {
+            FileReader fileReader = new FileReader("src/dev/jacbes/importUsers.txt");
+            Scanner scanner = new Scanner(fileReader);
+
+            while (scanner.hasNextLine()) {
+                List<String> information = Arrays.asList(scanner.nextLine().split(";"));
+                if (information.size() == 4) {
+                    Person person = new Person(information.get(0), information.get(1), information.get(2), information.get(3));
+                    System.out.println(person);
+                    person.call();
+                }
+                if (information.size() == 3) {
+                    Company company = new Company(information.get(0), information.get(1), information.get(2));
+                    System.out.println(company);
+                    company.call();
+                }
+            }
+
+            fileReader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
